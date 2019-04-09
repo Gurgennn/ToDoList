81ed28571e9426f0c6c25cb00dd3bdf3e1295599
gurgennn <gmnatsakanyann@gmail.com>
2019-04-09 17:24:23 +0400

    Add List Add constructors

--- ToDoList/ToDoList/ToDoLst.cs
+++ ToDoList/ToDoList/ToDoLst.cs
@@ -1,8 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ToDoList
 {
     public class ToDoLst
     {
+        private List<string> todos;
+
+        public ToDoLst(List<string> todos)
+        {
+            this.todos = todos;
+        }
+        public ToDoLst()
+        {
+            
+        }
     }
 }
