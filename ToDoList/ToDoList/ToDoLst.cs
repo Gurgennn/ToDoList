6765e4e0984773acd4ed5ff22a33df5b2c89e275
gurgennn <gmnatsakanyann@gmail.com>
2019-04-09 17:58:49 +0400

    Add (Add),Remove,RemoveAll,Complete,CompleteAll  functions

--- ToDoList/ToDoList/ToDoLst.cs
+++ ToDoList/ToDoList/ToDoLst.cs
@@ -13,7 +13,38 @@ namespace ToDoList
         }
         public ToDoLst()
         {
-            
+        }
+        public void Add(Task task)
+        {
+            todos.Add(task);
+        }
+        public void Remove(Task task)
+        {
+            todos.Remove(task);
+        }
+        public void Remove(int i)
+        {
+            todos.RemoveAt(i);
+        }
+        public int RemoveAll(Predicate<Task> match)
+        {
+            return todos.RemoveAll(match);
+        }
+        public void Complete(int i)
+        {
+            todos[i].Completeted = true;
+        }
+        public void CompleteAll(Predicate<Task> match)
+        {
+            for (int i = 0; i < todos.Count; i++)
+            {
+
+                if (match(todos[i]) == true)
+                {
+                    todos[i].Completeted = true;
+                }
+                
+            }
         }
     }
 }
